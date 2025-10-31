6e54a540333a39eb029a9f409877f2b3480ca77d
Orest Tokovenko <hi@oresttokovenko.com>
2025-10-30 20:42:34 -0600

    updating venv selector to use shorter path

--- .config/nvim/lua/plugins/venv-selector.lua
+++ .config/nvim/lua/plugins/venv-selector.lua
@@ -1,3 +1,8 @@
+local function shorter_name(filename) return filename:gsub(os.getenv("HOME"), "~"):gsub("/bin/python", "") end
+
 require("venv-selector").setup({
   auto_refresh = true,
+  options = {
+    on_telescope_result_callback = shorter_name,
+  },
 })
